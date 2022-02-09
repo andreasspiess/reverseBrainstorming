c9d3aa0c4f35ffe7981fb7edce091434c192c714
Your Name <you@example.com>
2022-02-09 12:21:39 +0100

    reverseBrainstormingStep5

--- src/main/java/com/example/reverseBrainstorming/HeadController.java
+++ src/main/java/com/example/reverseBrainstorming/HeadController.java
@@ -3,6 +3,7 @@ package com.example.reverseBrainstorming;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,16 +11,20 @@ import java.util.List;
 @Controller
 public class HeadController {
 
-    private List<String> positiv = new ArrayList<>();
-    private List<String> negativ = new ArrayList<>();
-    private String problem;
+    private List<String> positivList = new ArrayList<>();
+    private List<String> negativList = new ArrayList<>();
 
     @GetMapping("/")
     public String getStartPage(Model model) {
         model.addAttribute("eingabe");
-       // negativ.add()
+        // negativ.add()
         return "stepOne";
     }
 
+    @PostMapping("/")
+    public String saveProblem(Model model) {
+
+        return "stepOne";
+    }
 
 }
--- src/main/java/com/example/reverseBrainstorming/Problem.java
+++ src/main/java/com/example/reverseBrainstorming/Problem.java
@@ -1,6 +1,6 @@
 package com.example.reverseBrainstorming;
 
-public class Main {
+public class Problem {
 
 
 }
